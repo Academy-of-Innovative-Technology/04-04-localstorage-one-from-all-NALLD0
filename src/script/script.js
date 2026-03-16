4910c12f75a64e0102eafe1e971c498eaa1c24db
NALLD0 <naldon@nycstudents.net>
2026-03-16 01:28:00 -0400

    completed

--- src/script/script.js
+++ src/script/script.js
@@ -5,6 +5,19 @@ var Database = {
 
 function loadDataSource() {
 
+  var storeData = localStorage.getItem(Database.keyName);
+
+  if (storeData) {
+    Database.data = JSON.parse(storeData).response;
+  } else {
+    Database.data = [];
+  }
+
+  var selectHTML = '<option value="-1" selected> </option>';
+  Database.data.forEach(function(mutant, index) {
+    selectHTML += `<option value="${index}">${mutant.name.alias}</option>`;
+  });
+  document.querySelector('#select-mutant').innerHTML = selectHTML;
 
 }
 
@@ -13,9 +26,11 @@ function searchForMutantByAlias(mutantAlias) {
 
   var index = -1;
 
-
-
-
+  Database.data.forEach(function(mutant, i) {
+    if (mutant.name.alias === mutantAlias) {
+      index = i;
+    }
+  });
 
 
   if (index === -1) {
@@ -28,12 +43,26 @@ function searchForMutantByAlias(mutantAlias) {
 
 function displayData(index) {
 
-  var htmlTemplate = ``;
+  var mutant = Database.data[index];
 
-
-
-
-
+  var htmlTemplate = `<div class="card mb-4">
+      <div class="row g-0">
+        <div class="col-md-4 text-center p-3">
+          <img src="${mutant.image}" class="img-fluid rounded" alt="${mutant.name.alias}">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h3 class="card-title">${mutant.name.alias} (${mutant.name.firstName} ${mutant.name.lastName})</h3>
+            <p><strong>Gender:</strong> ${mutant.profile.gender}</p>
+            <p><strong>Eyes:</strong> ${mutant.profile.eyes}</p>
+            <p><strong>Hair:</strong> ${mutant.profile.hair}</p>
+            <p><strong>Height:</strong> ${mutant.profile.height}</p>
+            <p><strong>Powers:</strong> ${mutant.powers.join(', ')}</p>
+            <p><strong>Affiliations:</strong> ${mutant.affiliation.join(', ')}</p>
+          </div>
+        </div>
+      </div>
+    </div>`;
 
 
 
@@ -46,10 +75,16 @@ function getSelectedValue() {
   const selectedOption = selectElement.options[selectElement.selectedIndex];
   const selectedText = selectedOption.text;
 
-  // TEST METHOD // REMOVE OR HIDE
-  alert(selectedText);
 
-  searchForMutantByAlias(selectedText);
+  if (selectedOption.value !== "-1") {
+    searchForMutantByAlias(selectedText);
+  } else {
+    document.querySelector("#results-section").innerHTML = "";
+  }
+
+
+  // TEST METHOD // REMOVE OR HIDE
+
 
 }
 
